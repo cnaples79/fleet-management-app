43b57af18c609c1021f6c173bcf389b8d90fadce
cnaples79 <cnaples79@gmail.com>
2024-07-13 04:39:29 -0400

    Update Map.tsx

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,19 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import dynamic from 'next/dynamic';
 
-// Dynamically import Leaflet components only on the client side
-const MapContainer = dynamic(() => import('react-leaflet').then(mod => mod.MapContainer), { ssr: false });
-const TileLayer = dynamic(() => import('react-leaflet').then(mod => mod.TileLayer), { ssr: false });
-const Marker = dynamic(() => import('react-leaflet').then(mod => mod.Marker), { ssr: false });
-const Popup = dynamic(() => import('react-leaflet').then(mod => mod.Popup), { ssr: false });
-
-import 'leaflet/dist/leaflet.css';
-import L from 'leaflet';
-
-// Import marker icons dynamically to avoid SSR issues
-const iconUrl = require('leaflet/dist/images/marker-icon.png');
-const shadowUrl = require('leaflet/dist/images/marker-shadow.png');
-
+// Types for our props
 interface MapProps {
   vehicles: Array<{
     id: number;
@@ -25,45 +13,14 @@ interface MapProps {
   }>;
 }
 
+// Client-side only component
+const ClientSideMap = dynamic(() => import('./ClientSideMap'), {
+  ssr: false,
+  loading: () => <p>Loading map...</p>
+});
+
 const Map: React.FC<MapProps> = ({ vehicles }) => {
-  const center: [number, number] = [40.7128, -74.0060]; // New York City coordinates
-  const [defaultIcon, setDefaultIcon] = useState<L.Icon>();
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      // Create and set the default icon only on the client-side
-      const DefaultIcon = L.icon({
-        iconUrl: iconUrl,
-        shadowUrl: shadowUrl,
-        iconSize: [25, 41],
-        iconAnchor: [12, 41]
-      });
-      L.Marker.prototype.options.icon = DefaultIcon;
-      setDefaultIcon(DefaultIcon);
-    }
-  }, []);
-
-  return (
-    <div>
-      {defaultIcon && (
-        <MapContainer center={center} zoom={10} style={{ height: '400px', width: '100%' }}>
-          <TileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          />
-          {vehicles.map((vehicle) => (
-            <Marker
-              key={vehicle.id}
-              position={[vehicle.lastLocation.latitude, vehicle.lastLocation.longitude]}
-              icon={defaultIcon}
-            >
-              <Popup>{vehicle.name}</Popup>
-            </Marker>
-          ))}
-        </MapContainer>
-      )}
-    </div>
-  );
+  return <ClientSideMap vehicles={vehicles} />;
 };
 
 export default Map;
