1ce3346717547278beaddb3be8e00901ce343e2a
cnaples79 <cnaples79@gmail.com>
2024-07-13 04:31:43 -0400

    Update Map.tsx

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,11 +1,18 @@
 import React, { useEffect, useState } from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import dynamic from 'next/dynamic';
+
+// Dynamically import Leaflet components only on the client side
+const MapContainer = dynamic(() => import('react-leaflet').then(mod => mod.MapContainer), { ssr: false });
+const TileLayer = dynamic(() => import('react-leaflet').then(mod => mod.TileLayer), { ssr: false });
+const Marker = dynamic(() => import('react-leaflet').then(mod => mod.Marker), { ssr: false });
+const Popup = dynamic(() => import('react-leaflet').then(mod => mod.Popup), { ssr: false });
+
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 
-// Fix for default marker icon
-import icon from 'leaflet/dist/images/marker-icon.png';
-import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+// Import marker icons dynamically to avoid SSR issues
+const iconUrl = require('leaflet/dist/images/marker-icon.png');
+const shadowUrl = require('leaflet/dist/images/marker-shadow.png');
 
 interface MapProps {
   vehicles: Array<{
@@ -19,26 +26,26 @@ interface MapProps {
 }
 
 const Map: React.FC<MapProps> = ({ vehicles }) => {
-  const [mapLoaded, setMapLoaded] = useState(false);
   const center: [number, number] = [40.7128, -74.0060]; // New York City coordinates
+  const [defaultIcon, setDefaultIcon] = useState<L.Icon>();
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
-      let DefaultIcon = L.icon({
-        iconUrl: icon,
-        shadowUrl: iconShadow,
+      // Create and set the default icon only on the client-side
+      const DefaultIcon = L.icon({
+        iconUrl: iconUrl,
+        shadowUrl: shadowUrl,
         iconSize: [25, 41],
         iconAnchor: [12, 41]
       });
-
       L.Marker.prototype.options.icon = DefaultIcon;
-      setMapLoaded(true);
+      setDefaultIcon(DefaultIcon);
     }
   }, []);
 
   return (
     <div>
-      {mapLoaded && (
+      {defaultIcon && (
         <MapContainer center={center} zoom={10} style={{ height: '400px', width: '100%' }}>
           <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -48,10 +55,9 @@ const Map: React.FC<MapProps> = ({ vehicles }) => {
             <Marker
               key={vehicle.id}
               position={[vehicle.lastLocation.latitude, vehicle.lastLocation.longitude]}
+              icon={defaultIcon}
             >
-              <Popup>
-                {vehicle.name}
-              </Popup>
+              <Popup>{vehicle.name}</Popup>
             </Marker>
           ))}
         </MapContainer>
