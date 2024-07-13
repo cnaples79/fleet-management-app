6ba5c1677adb85e01a2f80c1f92218fb00fa6816
cnaples79 <cnaples79@gmail.com>
2024-07-13 06:16:29 -0400

    Update ClientSideMap.tsx

--- components/ClientSideMap.tsx
+++ components/ClientSideMap.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 interface Vehicle {
   id: number;
@@ -14,17 +14,24 @@ interface ClientSideMapProps {
 }
 
 const ClientSideMap: React.FC<ClientSideMapProps> = ({ vehicles }) => {
+  useEffect(() => {
+    console.log('ClientSideMap component mounted, vehicles:', vehicles);
+  }, [vehicles]);
+
   console.log('ClientSideMap rendering, vehicles:', vehicles);
 
   return (
     <div style={{ height: '400px', width: '100%', border: '1px solid black' }}>
       <h2>Map Placeholder</h2>
       <ul>
-        {vehicles.map((vehicle) => (
-          <li key={vehicle.id}>
-            {vehicle.name}: Lat {vehicle.lastLocation.latitude}, Lon {vehicle.lastLocation.longitude}
-          </li>
-        ))}
+        {vehicles.map((vehicle) => {
+          console.log('Rendering vehicle:', vehicle);
+          return (
+            <li key={vehicle.id}>
+              {vehicle.name}: Lat {vehicle.lastLocation.latitude}, Lon {vehicle.lastLocation.longitude}
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
