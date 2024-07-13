35c1f377ae9679de377756dbe65f27ccf62c21ec
cnaples79 <cnaples79@gmail.com>
2024-07-13 05:41:04 -0400

    Update ClientSideMap.tsx

--- components/ClientSideMap.tsx
+++ components/ClientSideMap.tsx
@@ -1,16 +1,6 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-
-// Fix the missing icon issue in Leaflet
-delete L.Icon.Default.prototype._getIconUrl;
-
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png').default,
-  iconUrl: require('leaflet/dist/images/marker-icon.png').default,
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png').default,
-});
 
 // Types for our props
 interface ClientSideMapProps {
