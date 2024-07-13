84dc5cf927c244d384ab133c53913eb305a85f2d
cnaples79 <cnaples79@gmail.com>
2024-07-13 06:27:17 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app';
 import { ErrorBoundary } from '../components/ErrorBoundary';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  console.log('MyApp rendering');
   return (
     <ErrorBoundary>
       <Component {...pageProps} />
@@ -10,11 +11,14 @@ function MyApp({ Component, pageProps }: AppProps) {
   );
 }
 
-// Add this error handler
 if (typeof window !== 'undefined') {
   window.onerror = (message, source, lineno, colno, error) => {
     console.error('Global error caught:', { message, source, lineno, colno, error });
   };
+
+  window.addEventListener('unhandledrejection', (event) => {
+    console.error('Unhandled promise rejection:', event.reason);
+  });
 }
 
 export default MyApp;
