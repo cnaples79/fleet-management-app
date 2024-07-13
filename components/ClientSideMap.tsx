c723f31f30a434ef97ff13dec7209b3643b19770
cnaples79 <cnaples79@gmail.com>
2024-07-13 06:10:27 -0400

    Update ClientSideMap.tsx

--- components/ClientSideMap.tsx
+++ components/ClientSideMap.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 
+interface Vehicle {
+  id: number;
+  name: string;
+  lastLocation: {
+    latitude: number;
+    longitude: number;
+  };
+}
+
 interface ClientSideMapProps {
-  vehicles: Array<{
-    id: number;
-    name: string;
-    lastLocation: {
-      latitude: number;
-      longitude: number;
-    };
-  }>;
+  vehicles: Vehicle[];
 }
 
 const ClientSideMap: React.FC<ClientSideMapProps> = ({ vehicles }) => {
