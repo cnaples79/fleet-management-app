bac883c17e069614ff71304e93665b8100f47fa6
cnaples79 <cnaples79@gmail.com>
2024-07-13 00:01:06 -0400

    Update Layout.tsx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,1 +1,24 @@
+import React from 'react';
+import Navbar from './Navbar';
 
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <main className="flex-grow container mx-auto px-4 py-8">
+        {children}
+      </main>
+      <footer className="bg-gray-800 text-white py-4">
+        <div className="container mx-auto text-center">
+          © 2024 Fleet Management App
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Layout;
