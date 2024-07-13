0e40f025933e789213032bb42c9d188e58d3e6ef
cnaples79 <cnaples79@gmail.com>
2024-07-13 07:02:32 -0400

    Update Map.tsx

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,43 +1,26 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import dynamic from 'next/dynamic';
-import { ErrorBoundary } from './ErrorBoundary';
 
-interface Vehicle {
-  id: number;
-  name: string;
-  lastLocation: {
-    latitude: number;
-    longitude: number;
-  };
-}
-
+// Types for our props
 interface MapProps {
-  vehicles: Vehicle[];
+  vehicles: Array<{
+    id: number;
+    name: string;
+    lastLocation: {
+      latitude: number;
+      longitude: number;
+    };
+  }>;
 }
 
+// Client-side only component
 const ClientSideMap = dynamic(() => import('./ClientSideMap'), {
   ssr: false,
-  loading: () => <p>Loading map...</p>
+  loading: () => <p>Loading map...</p>,
 });
 
 const Map: React.FC<MapProps> = ({ vehicles }) => {
-  useEffect(() => {
-    console.log('Map component mounted, vehicles:', vehicles);
-  }, [vehicles]);
-
-  if (!vehicles || vehicles.length === 0) {
-    console.log('No vehicle data available in Map component');
-    return <div>No vehicle data available.</div>;
-  }
-
-  return (
-    <ErrorBoundary>
-      <div>
-        <p>Attempting to render ClientSideMap...</p>
-        <ClientSideMap vehicles={vehicles} />
-      </div>
-    </ErrorBoundary>
-  );
+  return <ClientSideMap vehicles={vehicles} />;
 };
 
 export default Map;
