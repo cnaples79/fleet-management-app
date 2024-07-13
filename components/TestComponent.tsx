dd044a413e7b86cf6161c3d52c73e7f5804ab3be
cnaples79 <cnaples79@gmail.com>
2024-07-13 06:23:52 -0400

    Create TestComponent.tsx

---
+++ components/TestComponent.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const TestComponent: React.FC = () => {
+  console.log('TestComponent rendering');
+  return <div>Test Component</div>;
+};
+
+export default TestComponent;
